138fc06fc1fae307d2b1fd7e1eaa8bed9f2c48d7
Stephen J Bradshaw <stephen.j.bradshaw@gmail.com>
2020-10-08 16:46:49 +0100

    Modify VoteButton.jsx

--- src/components/VoteButton.jsx
+++ src/components/VoteButton.jsx
@@ -1,27 +1,54 @@
 import React from "react";
-import downArrow from "../img/DownArrow.svg";
 import upArrow from "../img/UpArrow.svg";
+import upArrowSelected from "../img/UpArrowSelected.svg";
+import upArrowDisabled from "../img/UpArrowDisabled.svg";
+import downArrow from "../img/DownArrow.svg";
+import downArrowSelected from "../img/DownArrowSelected.svg";
+import downArrowDisabled from "../img/DownArrowDisabled.svg";
 import { UserContext } from "../UserContext";
 
 const VoteButton = (props) => {
-  const { kind, updateVoteOptimistic } = props;
+  const { kind, updateVoteOptimistic, optimisticVotes } = props;
   return (
     <>
       <UserContext.Consumer>
         {({ user }) => {
-          return (
-            <input
-              type="image"
-              src={kind === "up" ? upArrow : downArrow}
-              width="20"
-              height="20"
-              aria-label={kind === "up" ? "upvote" : "downvote"}
-              disabled={!user}
-              onClick={(event) =>
-                updateVoteOptimistic(kind === "up" ? "up" : "down")
-              }
-            />
-          );
+          if (kind === "up")
+            return (
+              <input
+                type="image"
+                src={
+                  !user
+                    ? upArrowDisabled
+                    : optimisticVotes === 1
+                    ? upArrowSelected
+                    : upArrow
+                }
+                width="20"
+                height="20"
+                aria-label={"upvote"}
+                disabled={!user}
+                onClick={(event) => updateVoteOptimistic("up")}
+              />
+            );
+          else if (kind === "down")
+            return (
+              <input
+                type="image"
+                src={
+                  !user
+                    ? downArrowDisabled
+                    : optimisticVotes === -1
+                    ? downArrowSelected
+                    : downArrow
+                }
+                width="20"
+                height="20"
+                aria-label={"downvote"}
+                disabled={!user}
+                onClick={(event) => updateVoteOptimistic("down")}
+              />
+            );
         }}
       </UserContext.Consumer>
     </>
